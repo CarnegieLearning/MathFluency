5c1bc6f061b5fd988f1b0d06c808d0884ff0556f
Daniel Dickison <ddickison@carnegielearning.com>
2011-06-22 12:50:49 -0400

    Refine docs output and add the doc directory to .gitignore for now until we actually need to customize the documentation input files.

--- build-docs.sh
+++ build-docs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-mkdir -p doc
-NaturalDocs -i src -o HTML doc -p .
+OUTPUT=doc/naturaldocs-html
+PROJECT=doc/naturaldocs-project
+
+mkdir -p "$OUTPUT"
+mkdir -p "$PROJECT"
+NaturalDocs -i src -o HTML "$OUTPUT" -p "$PROJECT"
