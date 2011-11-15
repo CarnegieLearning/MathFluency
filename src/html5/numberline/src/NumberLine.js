9d9d3b75a2aa979f3a308b19bb5c600328ce1c62
John Conomikes <tempjc@carnegielearning.com>
2011-11-15 16:13:44 -0500

    Changed to support Hashmark class instead of manually drawing hashmarks

--- src/html5/numberline/src/NumberLine.js
+++ src/html5/numberline/src/NumberLine.js
@@ -15,23 +15,20 @@ Copyright 2011, Carnegie Learning
 */
 
 var cocos = require('cocos');
+var geo = require('geometry');
 
 // Visually represents the number line
 var NumberLine = cocos.nodes.Node.extend({
 	hashes		: [],		// Percentage based locations of hash marks
 	contents	: [],		// Holds Content under hash marks
 	init: function (hashes, values) {
-		// Ensure that the left hand end point exists
-		if(hashes[0] != 0) {
-			hashes.unshift(0);
-		}
-		
-		// Ensure that the right hand end point exists
-		if(hashes[hashes.length - 1] != 1) {
-			hashes.push(1);
-		}
-		
 		this.hashes = hashes;
+        this.set('z-order', 0);
+        
+        for(var i=0; i<this.hashes.length; i+=1) {
+            this.hashes[i].set('position', new geo.Point(this.hashes[i].location * 500, 0));
+            this.addChild({child: this.hashes[i]});
+        }
 		
 		// Set up the end point content
 		var temp;
@@ -45,26 +42,16 @@ var NumberLine = cocos.nodes.Node.extend({
 			}
 		}
 	},
+    
 	draw: function () {
 		// Draw the number line
 		context.strokeStyle = this.get('lineColor');
-		context.lineWidth = 4;
+		context.lineWidth = 6;
         context.beginPath();
         context.moveTo(-320, 0);
         context.lineTo(320, 0);
         context.closePath();
         context.stroke();
-		
-		context.lineWidth = 2;
-		
-		// Draw hash marks
-		for(var i=0; i<this.hashes.length; i+=1) {
-			context.beginPath();
-			context.moveTo(-300 + hashes[i] * 600,  5);
-			context.lineTo(-300 + hashes[i] * 600, -5);
-			context.closePath();
-			context.stroke();
-		}
 	},
 });
 
