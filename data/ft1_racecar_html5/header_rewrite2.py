2b16afc83edc98c712847e0cba56eb0408892f9d
John Conomikes <tempjc@carnegielearning.com>
2011-11-17 15:21:35 -0500

    Modified stages for carlow testing.

--- data/ft1_racecar_html5/header_rewrite2.py
+++ data/ft1_racecar_html5/header_rewrite2.py
@@ -7,14 +7,14 @@ def getFile(filename):
         f.close()
         return file
 
-j = 0
-while(j < 5):
+j = -1
+while(j < 4):
     j+= 1
     i = 0
     while(i < 40):
         i += 1
         
-        file = getFile('level04stage' + str(j) + '/l04_s' + str(j) + '_set' + str(i).zfill(3) + '.xml')
+        file = getFile('carlow_level03stage' + str(j) + '/l03_s' + str(j) + '_set' + str(i).zfill(3) + '.xml')
         
         output = []
         
@@ -22,7 +22,7 @@ while(j < 5):
         file[4] = file[4].replace('69000', '79000')
         file[5] = file[5].replace('89000', '102000')
 
-        f = open('level04stage' + str(j) + '/l04_s' + str(j) + '_set' + str(i).zfill(3) + '.xml', 'w')
+        f = open('carlow_level03stage' + str(j) + '/l03_s' + str(j) + '_set' + str(i).zfill(3) + '.xml', 'w')
         
         for line in file:
             f.write(line)
