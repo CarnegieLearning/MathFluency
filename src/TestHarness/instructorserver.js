ccc780a5ecd49eb88757f2a1aa33874007abc487
Tristan Nixon <tnixon@carnegielearning.com>
2011-11-30 18:36:27 +0000

    porting fix over from master - using forEachSeries instead of forEachLimit

--- src/TestHarness/instructorserver.js
+++ src/TestHarness/instructorserver.js
@@ -314,7 +314,7 @@ exports.addInstructorEndpoints = function (app, rootPath, gc, model, config)
         {
             console.log('Copying ' + questionResults.length + ' game output files');
             
-            async.forEachLimit(questionResults, 100,
+            async.forEachSeries(questionResults,
             function (item, callback)
             {
                 var inputPath = path.join(config.outputPath, item.dataFile);
