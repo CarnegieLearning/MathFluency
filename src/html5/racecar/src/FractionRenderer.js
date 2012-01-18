c11de1e818e81aea6554687a8303c824d6620f89
John Conomikes <tempjc@carnegielearning.com>
2012-01-18 13:36:39 -0500

    Unreverted FractionRenderer, now displaying mixed numbers properly

--- src/html5/racecar/src/FractionRenderer.js
+++ src/html5/racecar/src/FractionRenderer.js
@@ -20,8 +20,9 @@ var util = require('util');
 
 // TODO: Subclass this off a Value/Expression class or have it pulled in when needed by such a class
 var FractionRenderer = cocos.nodes.Node.extend({
-    numerator   : 1,           // The numerator of the fraction
+    numerator   : 1,            // The numerator of the fraction
     denominator : 2,            // The denominator of the fraction
+    whole       : null,         // The mixed number component of the fraction
     bgColor     : '#fff',       // Color of the background rectangle
     fontColor   : '#000',       // Color of the numerator and denominator (TODO: Seperate for numerator/denominator?)
     fontSize    : '16',         // Size of the numerator and denominator text
@@ -31,7 +32,7 @@ var FractionRenderer = cocos.nodes.Node.extend({
         FractionRenderer.superclass.init.call(this);
         
         // Set properties from the option object
-        util.each('numerator denominator bgColor fontColor seperatorColor fontName fontColor fontSize'.w(), util.callback(this, function (name) {
+        util.each('numerator denominator whole bgColor fontColor seperatorColor fontName fontColor fontSize'.w(), util.callback(this, function (name) {
             if (opts[name]) {
                 this.set(name, opts[name]);
             }
@@ -43,18 +44,47 @@ var FractionRenderer = cocos.nodes.Node.extend({
         opts["fontName"] = this.get("fontName");
         opts["fontColor"] = this.get("fontColor");
         opts["fontSize"] = this.get("fontSize");
-        var label = cocos.nodes.Label.create(opts);
-        label.set('position', new geom.Point(0, -15));
-        label.bindTo('opacity', this, 'opacity');
-        this.addChild({child: label});
+        
+        var n = cocos.nodes.Label.create(opts);
+        n.bindTo('opacity', this, 'opacity');
+        this.addChild({child: n});
         
         opts["string"] = this.get("denominator");
-        label = cocos.nodes.Label.create(opts);
-        label.set('position', new geom.Point(0, 15));
-        label.bindTo('opacity', this, 'opacity');
-        this.addChild({child: label});
+        var d = cocos.nodes.Label.create(opts);
+        d.bindTo('opacity', this, 'opacity');
+        this.addChild({child: d});
         
-        this.set('contentSize', new geom.Size(40, 50));
+        // Figuring out combined content size
+        var v = n.get('contentSize').height / 2 + d.get('contentSize').height / 2 + 36;
+        var h = Math.max(n.get('contentSize').width, d.get('contentSize').width) + 10;
+        
+        // Regular fraction defaults
+        if(this.get('whole') == null) {
+            n.set('position', new geom.Point(0, -15));
+            d.set('position', new geom.Point(0, 15));
+            
+            this.set('contentSize', new geom.Size(h, v));
+        }
+        // Account for the inclusion of a mixed number
+        else {
+            opts["string"] = this.get("whole");
+            opts["fontSize"] *= 2;
+            var w = cocos.nodes.Label.create(opts);
+            w.bindTo('opacity', this, 'opacity');
+            this.addChild({child: w});
+            
+            n.set('anchorPoint', new geom.Point(1, 0.5));
+            n.set('position', new geom.Point(h / 2 + 2, -15));
+            d.set('anchorPoint', new geom.Point(1, 0.5));
+            d.set('position', new geom.Point(h / 2 + 2, 15));
+            
+            w.set('anchorPoint', new geom.Point(0, 0.5));
+            w.set('position', new geom.Point(h / -2 - 2, 0));
+            
+            h += w.get('contentSize').width;
+            
+            this.set('contentSize', new geom.Size(h, v));
+        }
     },
     
     // Draw the background and the horizontal fraction bar
@@ -72,23 +102,18 @@ var FractionRenderer = cocos.nodes.Node.extend({
         
         context.strokeStyle = this.get('lineColor');
         context.beginPath();
-        context.moveTo(size.height /  4, 0);
-        context.lineTo(size.height / -4, 0);
+        if(this.get('whole') == null) {
+            context.moveTo(size.width / -4, 0);
+            context.lineTo(size.width /  4, 0);
+        }
+        // Account for offset due to mixed number presence
+        else {
+            context.moveTo(2                 , 0);
+            context.lineTo(size.width / 2 - 2, 0);
+        }
         context.closePath();
         context.stroke();
     },
 });
-
-// Static helper function to build the creation options object
-FractionRenderer.helper = function(n, d, b, t, s) {
-    var opts = new Object();
-    opts['numerator'] = n;
-    opts['denominator'] = d;
-    opts['bgColor'] = b;
-    opts['fontColor' ] = t;
-    opts['lineColor'] = s;
-    
-    return opts;
-}
 
 exports.FractionRenderer = FractionRenderer
