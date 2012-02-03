fac58a5a455adcd6dffeea4a0c5879f2149eafe7
John Conomikes <tempjc@carnegielearning.com>
2012-02-03 13:26:17 -0500

    Merge of Firefox 5 safe changes over from snowboard

--- src/html5/numberline/src/AudioTrack.js
+++ src/html5/numberline/src/AudioTrack.js
@@ -19,25 +19,27 @@ var cocos = require('cocos2d')
 
 // Wrapper class for the <audio> element
 var AudioTrack = BObject.extend({
-    audio       : null,     // Holds the actual audio element
-    loop        : false,    // Whether or not the track should loop on ending
-    isPlaying   : false,    // True if the track is current playing
-    volume      : 1,        // [0-1] the volume for this specific AudioTrack
-    masterVolume: 1,        // [0-1] the volume of the AudioMixer
+    audio : null, // Holds the actual audio element
+    loop : false, // Whether or not the track should loop on ending
+    isPlaying : false, // True if the track is current playing
+    volume : 1, // [0-1] the volume for this specific AudioTrack
+    masterVolume: 1, // [0-1] the volume of the AudioMixer
     init: function(file) {
         this.audio = document.createElement('audio');
         this.audio.setAttribute('src', file);
-        this.audio.onended = this.endCallback.bind(this);
+        this.audio.addEventListener('ended', this.endCallback.bind(this), false);
         this.set('isPlaying', false);
     },
     
     // Starts playing the audio if it is not already playing
     // Returns true if the audio started to play
     play: function() {
-        if(!this.get('isPlaying')) {
-            this.audio.play();
-            this.set('isPlaying', true);
-            return true;
+        if(this.audio.networkState != HTMLMediaElement.NETWORK_NO_SOURCE) {
+            if(!this.get('isPlaying')) {
+                this.audio.play();
+                this.set('isPlaying', true);
+                return true;
+            }
         }
         return false;
     },
@@ -51,11 +53,13 @@ var AudioTrack = BObject.extend({
     // Stops the audio if it is currently playing
     // Returns true if the audio was stopped
     stop: function() {
-        if(this.get('isPlaying')) {
-            this.audio.pause();
-            this.audio.currentTime = 0;
-            this.set('isPlaying', false);
-            return true;
+        if(this.audio.networkState != HTMLMediaElement.NETWORK_NO_SOURCE) {
+            if(this.get('isPlaying')) {
+                this.audio.pause();
+                this.audio.currentTime = 0;
+                this.set('isPlaying', false);
+                return true;
+            }
         }
         return false;
     },
@@ -71,22 +75,28 @@ var AudioTrack = BObject.extend({
     
     // Sets the muted attribute for the audio
     setMute: function(b) {
-        this.audio.muted = b;
+        if(this.audio.networkState != HTMLMediaElement.NETWORK_NO_SOURCE) {
+            this.audio.muted = b;
+        }
     },
     
     // Called by AudioMixer when the master volume level is changed
     updateMasterVolume: function(v) {
-        this.set('masterVolume', v);
-        this.audio.volume = v * this.get('volume');
+        if(this.audio.networkState != HTMLMediaElement.NETWORK_NO_SOURCE) {
+            this.set('masterVolume', v);
+            this.audio.volume = v * this.get('volume');
+        }
     },
     
     // Called to change the volume of this specific AudioTrack
     setVolume: function(v) {
-        // Keep the volume level within acceptable range
-        v = Math.min(Math.max(0, v), 1);
-        
-        this.set('volume', v);
-        this.audio.volume = this.get('masterVolume') * v;
+        if(this.audio.networkState != HTMLMediaElement.NETWORK_NO_SOURCE) {
+            // Keep the volume level within acceptable range
+            v = Math.min(Math.max(0, v), 1);
+            
+            this.set('volume', v);
+            this.audio.volume = this.get('masterVolume') * v;
+        }
     }
 });
 
